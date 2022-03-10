5e4f757c17291914ff8aafc028bfcb547af96c8e
IvanBogrec <dotagamer@nara-clan.ru>
2022-03-10 16:44:18 +0300

    ClassCommit

---
+++ Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticalWork1
+{
+    class Product
+    {
+        public string PrName { get; set; }
+        public string PrCipher { get; set; }
+        public int PrNumber { get; set; }
+
+        public Product() { }
+
+        public Product(string PrName, string PrCipher, int PrNumber)
+        {
+            PrName = this.PrName;
+            PrCipher = this.PrCipher;
+            PrNumber = this.PrNumber;
+        }
+    }
+}
